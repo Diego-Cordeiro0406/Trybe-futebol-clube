e9c8b600f097890db48fea8fddd202dc2deac203
diego cordeiro <diegocordeiro121@gmail.com>
2023-09-26 15:53:25 -0300

    TDD: adicionando a primeira partes dos testes da rota get/teams

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -3,11 +3,13 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
-import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import { App } from '../app';
+import SequelizeTeam from '../database/models/SequelizeTeam';
 
 import { Response } from 'superagent';
+import { teams } from './mocks/team.mock';
 
+const { app } = new App();
 chai.use(chaiHttp);
 
 const { expect } = chai;
@@ -38,6 +40,15 @@ describe('Seu teste', () => {
 
   //   expect(...)
   // });
+
+  it('Deve retornar todos os livros', async function() {
+    sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any);
+    const chaiHttpResponse = await chai.request(app).get('/teams')
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.deep.equal(teams);
+  });
+ afterEach(sinon.restore)
 
   it('Seu sub-teste', () => {
     expect(false).to.be.eq(true);
